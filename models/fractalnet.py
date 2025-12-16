8cbe6229b06df3963506b565983d29a8a5b1729e
Hyeonho Kim <hyeonho.kim@g.skku.edu>
2025-12-16 11:04:37 +0000

    modify fractalnet.py

--- models/fractalnet.py
+++ models/fractalnet.py
@@ -20,74 +20,75 @@ class ConvBlock(nn.Module):
             out = self.dropout(out)
         return out
 
+"""
+class FractalBlock():
+    def __init__(self, depth):
+        self.path1 = ConvBlock()
+        self.path2 = nn.ModuleList([FractalBlock(depth-1), FractalBlock(depth-1)])
+
+    def forward(self, x): # x -> [y1, y2, ..., yK], k = number of columns
+        y1 = self.path1(x)
+        [y2, ..., yK] = self.path2[0](x)
+        [y2, ..., yK] = self.path2[1](join(y2, ..., yK))
+        return [y1, ..., yK]
+"""
+
 class FractalBlock(nn.Module):
-    def __init__(self, C, in_channels, out_channels, dropout_rate, local_drop_rate=0.15): # C: number of columns
+    def __init__(self, depth, in_channels, out_channels, dropout_rate, local_drop_rate=0.15):
         super().__init__()
-        self.C = C
-        self.in_channels = in_channels
-        self.out_channels = out_channels
+        self.depth = depth
         self.local_drop_rate = local_drop_rate
 
-        if C == 1:
+        if depth == 1:
             self.path1 = ConvBlock(in_channels, out_channels, dropout_rate)
         else:
             self.path1 = ConvBlock(in_channels, out_channels, dropout_rate)
-            self.path2 = nn.ModuleList([FractalBlock(C-1, in_channels, out_channels, dropout_rate),
-                                        FractalBlock(C-1, out_channels, out_channels, dropout_rate)])
-    
-    def join(self, x1, x2, global_chosen_columns):
+            self.path2 = nn.ModuleList([FractalBlock(depth-1, in_channels, out_channels, dropout_rate),
+                                        FractalBlock(depth-1, out_channels, out_channels, dropout_rate)])
+
+    def join(self, y2_yK, global_chosen_columns): # y2_yK = [(B, C, H, W)_2, ..., (B, C, H, W)_K] -> element-wise mean (B, C, H, W)
         if not self.training:
-            return (x1+x2) / 2
+            return torch.mean(torch.stack(y2_yK), dim=0)    
         
-        B = x1.size(0) # B: batch size
+        B = y2_yK[0].size(0) # batch size.
+        num_inputs = len(y2_yK)
+        device = y2_yK[0].device
 
-        # 이번 배치가 global drop path인 경우.
+        # global drop path:
         if global_chosen_columns is not None:
-            global_mask = torch.zeros((B, 2), device=x1.device)
-            
-            global_mask[:, 0] = (global_chosen_columns == self.C).float()
-            global_mask[:, 1] = (global_chosen_columns < self.C).float()
+            column_indices = torch.arange(num_inputs, 0, -1, device=device).repeat(B, 1) # (B, num_inputs), 뒤에서 앞으로 1, 2, ... 증가. (e.g. [[3,2,1], [3,2,1], ...])
+            global_drop_mask = (column_indices == global_chosen_columns.view(B, 1)).float() # (B, num_inputs), 선택된 column에만 1. mask.
+            global_drop_mask = global_drop_mask.view(B, num_inputs, 1, 1, 1)
 
-            global_mask = global_mask.view(B, 2, 1, 1)
-
-            mask_x1 = global_mask[:, 0:1] # (B, 1, 1, 1)
-            mask_x2 = global_mask[:, 1:2] # (B, 1, 1, 1)
-
-            # global drop path는 (i) 둘 중 한 컬럼만 살아남거나, (ii) 둘 다 살아남지 못하거나.
-            return x1*mask_x1 + x2*mask_x2
-        # 이번 배치가 local drop path인 경우.
+            global_masked = torch.stack(y2_yK, dim=1) * global_drop_mask
+            return global_masked.sum(dim=1) / global_drop_mask.sum(dim=1).clamp(min=1) # join 연산에 모두 참여하지 않을경우, clamp 사용하여 0 / 1로 처리.
+        # local drop path:
         else:
             keep_prob = 1. - self.local_drop_rate
-            # local drop path 적용.
-            local_drop_mask = torch.bernoulli(torch.full((B, 2), keep_prob, device=x1.device)) # [B, 2]
+            local_drop_mask = torch.bernoulli(torch.full((B, num_inputs), keep_prob, device=device)) # [B, num_inputs]
             
+            # 모두 dead인 경우 처리.
             dead_mask = (local_drop_mask.sum(dim=1) == 0)
-            # 둘 다 dead인 경우 처리.
             if dead_mask.any():
                 dead_indices = torch.where(dead_mask)[0]
-                random_choices = torch.randint(0, 2, (dead_indices.size(0),), device=x1.device)
+                random_choices = torch.randint(0, num_inputs, (dead_indices.size(0),), device=device)
                 local_drop_mask[dead_indices, random_choices] = 1.
-            
-            # [B, 2, 1, 1]로 확장.
-            local_drop_mask = local_drop_mask.view(B, 2, 1, 1)
 
-            # 각 경로 마스크.
-            mask_x1 = local_drop_mask[:, 0:1] # [B, 1, 1, 1]
-            mask_x2 = local_drop_mask[:, 1:2] # [B, 1, 1, 1]
+            # mask 적용.
+            local_drop_mask = local_drop_mask.view(B, num_inputs, 1, 1, 1)
+            local_masked = torch.stack(y2_yK, dim=1) * local_drop_mask
 
-            num_alive = mask_x1 + mask_x2
+            return local_masked.sum(dim=1) / local_drop_mask.sum(dim=1)
 
-            return (x1*mask_x1 + x2*mask_x2) / num_alive
-
-    def forward(self, x, global_chosen_columns):
-        if self.C == 1:
-            return self.path1(x)
+    def forward(self, x, global_chosen_columns): # x -> [y1, y2, ..., yK], K = number of columns, x: (B, C_in, H, W)
+        if self.depth == 1:
+            out = [self.path1(x)]
         else:
-            out1 = self.path1(x)
-            out2_1 = self.path2[0](x, global_chosen_columns)
-            out2_2 = self.path2[1](out2_1, global_chosen_columns)
-            out = self.join(out1, out2_2, global_chosen_columns)
-            return out
+            y1 = [self.path1(x)] # (B, C_in, H, W) -> [(B, C_out, H, W)]
+            y2_yK = self.path2[0](x, global_chosen_columns)
+            y2_yK = self.path2[1](self.join(y2_yK, global_chosen_columns), global_chosen_columns)
+            out = y1 + y2_yK
+        return out
 
 class FractalNet(nn.Module):
     def __init__(self, num_columns=4, num_blocks=5):
@@ -104,10 +105,10 @@ class FractalNet(nn.Module):
             self.layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
 
             in_channels = out_channels[i]
-
+        
         self.layers.append(nn.Flatten())
         self.layers.append(nn.Linear(512*1*1, 10))
-    
+
     def forward(self, x):
         out = x
 
@@ -116,11 +117,12 @@ class FractalNet(nn.Module):
         if self.training:
             global_drop_ratio = 0.5
             if torch.rand(1).item() < global_drop_ratio: # 배치별로 global or local 선택.
-                global_chosen_columns = torch.randint(1, self.num_columns+1, (B,), device=x.device) # 샘플 별 컬럼 1개 선택.
+                global_chosen_columns = torch.randint(1, self.num_columns+1, (B,), device=x.device) # 샘플 별 컬럼 1개 선택. 오른쪽에서 왼쪽으로 1, 2, ...
 
         for layer in self.layers:
             if isinstance(layer, FractalBlock):
                 out = layer(out, global_chosen_columns)
+                out = torch.mean(torch.stack(out), dim=0) # block의 마지막 join.
             else:
                 out = layer(out)
         return out
