31f7b239cf4a43a71a9fdb7be4b1b46e05bf224c
Hyeonho Kim <hyeonho.kim@g.skku.edu>
2025-12-26 11:00:57 +0000

    apply methods module to main.py

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
 from datasets import load_dataset
 from models import *
+from methods import load_method
 import argparse
 from torch.utils.tensorboard import SummaryWriter
 import torch
+import torch.nn.functional as F
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0
 
-def train(args, criterion, optimizer, trainloader, model, writer, epoch):
-    model.train()
+def train(args, method, optimizer, trainloader, writer, epoch):
+    method.model.train()
+    
     train_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        if args.model == 'simclr':
-            xi, xj = data
-            xi, xj = xi.to(device), xj.to(device)
+    for batch in trainloader:
+        batch = tuple(b.to(device) for b in batch)
 
-            optimizer.zero_grad()
-            _, zi = model(xi) # contrastive 학습에는 feature h만아닌, proejction head output z만 사용.
-            _, zj = model(xj)
-            loss = criterion(zi, zj)
-        else:
-            inputs, labels = data[0].to(device), data[1].to(device) # cuda
+        optimizer.zero_grad()
+        loss = method(batch)
 
-            optimizer.zero_grad()
-
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-        
         loss.backward()
         optimizer.step()
 
@@ -35,9 +27,8 @@ def train(args, criterion, optimizer, trainloader, model, writer, epoch):
     print(f'Epoch {epoch} - Train Loss: {train_loss/len(trainloader):.4f}')
     writer.add_scalar('train_loss',train_loss/len(trainloader),epoch)
 
-
-def test(args, testloader, model, criterion, epoch, writer):
-    model.eval()
+def test(args, testloader, method, epoch, writer):
+    method.model.eval()
 
     test_loss = 0
     correct = 0
@@ -46,20 +37,20 @@ def test(args, testloader, model, criterion, epoch, writer):
         for data in testloader:
             images, labels = data[0].to(device), data[1].to(device) # cuda
 
-            outputs = model(images)
-            loss = criterion(outputs, labels)
+            outputs = method.model(images)
+            loss = F.cross_entropy(outputs, labels)
 
             test_loss += loss.item()
             _, predicted = torch.max(outputs, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    acc = 100.*correct/total
+    acc = 100. * correct / total
     global best_acc
     if acc > best_acc:
         best_acc = acc
 
-        save_ckpt(args, model, acc, epoch)
+        save_ckpt(args, method.model, acc, epoch)
 
     print(f'Epoch {epoch} - Test Loss: {test_loss/len(testloader):.4f}, Test Acc: {acc:.2f}%, Best Acc: {best_acc:.2f}')
     writer.add_scalar('test_loss',test_loss/len(testloader),epoch)
@@ -80,20 +71,21 @@ def _main(args):
     
     trainloader, testloader = load_dataset(args.dataset, args.batch_size)
 
-    criterion = load_criterion(args.criterion, args.batch_size)
+    method = load_method(args.method, model)
+    # criterion = load_criterion(args.criterion, args.batch_size)
     optimizer = load_optimizer(args.optimizer, model, args.lr, args.weight_decay, args.momentum, args.nesterov)
     scheduler = load_scheduler(args.scheduler, optimizer, args.num_epochs)
 
     writer = SummaryWriter('./runs/' + args.model)
 
     for epoch in range(args.num_epochs): # resnet은 64k iteration => 64000*128/5000 = 약 163 epoch.
-        train(args, criterion, optimizer, trainloader, model, writer, epoch)
+        train(args, method, optimizer, trainloader, writer, epoch)
         # pretrain시에 마지막 parameter일 경우 모델 저장.
         if args.pretrain:
             save_ckpt(args, model, -1, epoch)
         # SimCLR 같이 pretrain할 경우 test 진행 x.
         if testloader is not None:
-            test(args, testloader, model, criterion, epoch, writer)
+            test(args, testloader, method, epoch, writer)
         scheduler.step()
     
     writer.close()
@@ -102,10 +94,11 @@ def _main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str)
+    parser.add_argument('--method', type=str)
     parser.add_argument('--dataset', type=str, default='CIFAR10')
     parser.add_argument('--num_epochs', type=int)
     parser.add_argument('--batch_size', type=int)
-    parser.add_argument('--criterion', type=str, default='crossentropyloss')
+    # parser.add_argument('--criterion', type=str, default='crossentropyloss')
     parser.add_argument('--optimizer', type=str, default='SGD')
     parser.add_argument('--lr', type=float)
     parser.add_argument('--momentum', type=float, default=0.9)
